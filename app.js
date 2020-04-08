b41814297c43f50153c752d47c2ea30703bb72f2
HusamMaher <husamalmaher@gmail.com>
2020-04-09 00:27:37 +0300

    intializing

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const { ensureAuthenticated } = require('./helpers/auth');
+const app = express();
+
+// Load routes
+
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/moti-DB', {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+// Method override middleware
+app.use(methodOverride('_method'));
+
+// Express session midleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Index Route
+app.get('/', ensureAuthenticated, (req, res) => {
+
+  res.render('home');
+});
+
+
+// Use routes
+
+app.use('/users', users);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
